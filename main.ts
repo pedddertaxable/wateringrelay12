70a8a20b4bf8e47aa611945f32d946b11511259c
pedddertaxable <84002268+pedddertaxable@users.noreply.github.com>
2021-06-01 07:45:57 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,19 +4,29 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(WaterTime / 1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(RelayTrigger)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(WaterTime)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(RelayTrigger)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    RunCount += 1
+    basic.pause(WaterInterval)
 })
 input.onButtonPressed(Button.B, function () {
     WaterTime += -10 * 1000
     basic.showNumber(WaterTime / 1000)
 })
+let RelayTrigger = 0
+let WaterInterval = 0
 let WaterTime = 0
 pins.digitalWritePin(DigitalPin.P1, 1)
 pins.digitalWritePin(DigitalPin.P2, 1)
 let RunCount = 0
-WaterTime = 1 * (60 * 1000)
-let WateringTimeIncrease = 0
-let WaterInterval = 12 * (60 * (60 * 1000)) - WaterTime
-let RelayTrigger = 10000
+WaterTime = 2 * 60 * 1000
+WaterInterval = 12 * 60 * 60 * 1000 - WaterTime
+RelayTrigger = 10000
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     basic.pause(RelayTrigger)
@@ -27,6 +37,4 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P2, 1)
     RunCount += 1
     basic.pause(WaterInterval)
-    WaterInterval += -1 * WateringTimeIncrease
-    WaterTime += WateringTimeIncrease
 })
