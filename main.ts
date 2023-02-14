58d3c0167531404de44d0c6ab4c3b89c887356b5
pedddertaxable <84002268+pedddertaxable@users.noreply.github.com>
2023-02-14 20:15:33 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
-input.onButtonPressed(Button.A, function () {
-    WaterTime += 10 * 1000
-    basic.showNumber(WaterTime / 1000)
-})
+function FillBucket () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(PumpTime)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+}
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(WaterTime / 1000)
+    basic.showString("Waters ")
+    basic.showString("" + (NoOfWaters))
+    basic.pause(1000)
+    basic.clearScreen()
+})
+function Run () {
+    while (true) {
+        FillBucket()
+        WaterPlants()
+        NoOfWaters += 1
+        basic.pause(WaterInterval)
+    }
+}
+function WaterPlants () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     basic.pause(RelayTrigger)
     pins.digitalWritePin(DigitalPin.P1, 1)
@@ -11,32 +25,29 @@ input.onButtonPressed(Button.AB, function () {
     pins.digitalWritePin(DigitalPin.P2, 0)
     basic.pause(RelayTrigger)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    RunCount += 1
-    basic.pause(WaterInterval)
-})
-input.onButtonPressed(Button.B, function () {
-    WaterTime += -10 * 1000
-    basic.showNumber(WaterTime / 1000)
-})
-let RelayTrigger = 0
+}
 let WaterInterval = 0
 let WaterTime = 0
+let PumpTime = 0
+let RelayTrigger = 0
+let NoOfWaters = 0
+pins.digitalWritePin(DigitalPin.P0, 1)
 pins.digitalWritePin(DigitalPin.P1, 1)
 pins.digitalWritePin(DigitalPin.P2, 1)
-let RunCount = 0
-WaterTime = 2 * 60 * 1000
-WaterInterval = 24 * 60 * 60 * 1000 - WaterTime
+NoOfWaters = 0
 RelayTrigger = 10000
-basic.pause(5000)
+basic.showString("TestA/RunB")
 while (true) {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(RelayTrigger)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(WaterTime)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    basic.pause(RelayTrigger)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    RunCount += 1
-    basic.pause(WaterInterval)
-    WaterTime += 10000
+    if (input.buttonIsPressed(Button.A)) {
+        PumpTime = 5 * 1000
+        WaterTime = 5 * 1000
+        WaterInterval = 10 * 1000
+        Run()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        PumpTime = 60 * 60 * 1000
+        WaterTime = 5 * 60 * 1000
+        WaterInterval = 23 * 60 * 60 * 1000
+        Run()
+    }
 }
