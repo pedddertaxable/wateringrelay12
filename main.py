5a006bed86b70261a1e8e2036b5aa5c31736bc0a
pedddertaxable <84002268+pedddertaxable@users.noreply.github.com>
2023-02-14 20:38:10 +0000

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-def on_button_pressed_a():
-    basic.show_number(1)
-    pins.digital_write_pin(DigitalPin.P1, 0)
-    basic.pause(RelayTrigger)
-    pins.digital_write_pin(DigitalPin.P1, 1)
-    basic.clear_screen()
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def FillBucket():
+    pins.digital_write_pin(DigitalPin.P0, 0)
+    basic.pause(PumpTime)
+    pins.digital_write_pin(DigitalPin.P0, 1)
 
 def on_button_pressed_ab():
-    basic.show_number(RunCount)
+    basic.show_string("Waters ")
+    basic.show_string("" + str((NoOfWaters)))
+    basic.pause(1000)
+    basic.clear_screen()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def on_button_pressed_b():
-    basic.show_number(2)
-    pins.digital_write_pin(DigitalPin.P2, 0)
-    basic.pause(RelayTrigger)
-    pins.digital_write_pin(DigitalPin.P2, 1)
-    basic.clear_screen()
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-RelayTrigger = 0
-RunCount = 0
-pins.digital_write_pin(DigitalPin.P1, 1)
-pins.digital_write_pin(DigitalPin.P2, 1)
-RunCount = 0
-WaterTime = 2 * (60 * 1000)
-WateringTimeIncrease = 10000
-WaterInterval = 12 * (60 * (60 * 1000)) - WaterTime
-RelayTrigger = 10000
-
-def on_forever():
-    global RunCount, WaterInterval, WaterTime
+def Run():
+    global NoOfWaters
+    while True:
+        FillBucket()
+        WaterPlants()
+        NoOfWaters += 1
+        basic.pause(WaterInterval)
+def WaterPlants():
     pins.digital_write_pin(DigitalPin.P1, 0)
     basic.pause(RelayTrigger)
     pins.digital_write_pin(DigitalPin.P1, 1)
@@ -37,8 +25,25 @@ def on_forever():
     pins.digital_write_pin(DigitalPin.P2, 0)
     basic.pause(RelayTrigger)
     pins.digital_write_pin(DigitalPin.P2, 1)
-    RunCount += 1
-    basic.pause(WaterInterval)
-    WaterInterval += -1 * WateringTimeIncrease
-    WaterTime += WateringTimeIncrease
-basic.forever(on_forever)
+WaterInterval = 0
+WaterTime = 0
+PumpTime = 0
+RelayTrigger = 0
+NoOfWaters = 0
+pins.digital_write_pin(DigitalPin.P0, 1)
+pins.digital_write_pin(DigitalPin.P1, 1)
+pins.digital_write_pin(DigitalPin.P2, 1)
+NoOfWaters = 0
+RelayTrigger = 10000
+basic.show_string("TestA/RunB")
+while True:
+    if input.button_is_pressed(Button.A):
+        PumpTime = 5 * 1000
+        WaterTime = 5 * 1000
+        WaterInterval = 10 * 1000
+        Run()
+    if input.button_is_pressed(Button.B):
+        PumpTime = 60 * 60 * 1000
+        WaterTime = 5 * 60 * 1000
+        WaterInterval = 23 * 60 * 60 * 1000
+        Run()
--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 function FillBucket () {
     pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.showString("" + (TextList[0]))
     basic.pause(PumpTime)
     pins.digitalWritePin(DigitalPin.P0, 1)
 }
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("Waters ")
+    basic.showString("#Waters ")
     basic.showString("" + (NoOfWaters))
     basic.pause(1000)
     basic.clearScreen()
@@ -19,32 +20,52 @@ function Run () {
 }
 function WaterPlants () {
     pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.showString("" + (TextList[1]))
     basic.pause(RelayTrigger)
     pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.showString("" + (TextList[2]))
     basic.pause(WaterTime)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showString("" + (TextList[3]))
     basic.pause(RelayTrigger)
     pins.digitalWritePin(DigitalPin.P2, 1)
 }
+let TextList: string[] = []
 let WaterInterval = 0
 let WaterTime = 0
 let PumpTime = 0
 let RelayTrigger = 0
 let NoOfWaters = 0
+basic.showIcon(IconNames.Yes)
+basic.pause(100)
+basic.clearScreen()
 pins.digitalWritePin(DigitalPin.P0, 1)
 pins.digitalWritePin(DigitalPin.P1, 1)
 pins.digitalWritePin(DigitalPin.P2, 1)
 NoOfWaters = 0
 RelayTrigger = 10000
-basic.showString("TestA/RunB")
 while (true) {
     if (input.buttonIsPressed(Button.A)) {
+        basic.showString("Test")
         PumpTime = 5 * 1000
         WaterTime = 5 * 1000
         WaterInterval = 10 * 1000
+        TextList = [
+        "Pump",
+        "Open",
+        "Water",
+        "Close"
+        ]
         Run()
     }
     if (input.buttonIsPressed(Button.B)) {
+        basic.showString("Run")
+        TextList = [
+        "",
+        "",
+        "",
+        ""
+        ]
         PumpTime = 60 * 60 * 1000
         WaterTime = 5 * 60 * 1000
         WaterInterval = 23 * 60 * 60 * 1000
